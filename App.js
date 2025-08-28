2f2d4df971acf9eb92c048688687e91bbfa4746e
Marcoz-VS <marcozviniciussilva@gmail.com>
2025-08-28 10:41:40 -0300

    Tela Home do Aluno

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
+import HomeAlunoScreen from './screens/HomeAlunoScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <LoginScreen />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="HomeAluno" component={HomeAlunoScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
