40a5a1d9693f6a341ebc4f2d1ef7233368806249
Marcoz-VS <marcozviniciussilva@gmail.com>
2025-09-03 11:26:58 -0300

    logica do ticket (1)

--- screens/TicketScreen.jsx
+++ screens/TicketScreen.jsx
@@ -1,13 +1,23 @@
 import { useSelector } from 'react-redux';
 import { SafeAreaView, Text } from 'react-native';
+import CustomButton from '../components/CustomButton';
 
-export default function TicketScreen() {
+export default function TicketScreen(navigation) {
   const user = useSelector((state) => state.auth.user);
 
   return (
     <SafeAreaView>
       <Text>Ticket Screen</Text>
       <Text>Bem-vindo, {user.nome}!</Text>
+      <CustomButton 
+        title="Pegar Ticket" 
+        onPress={() => { /* Lógica para pegar ticket */ }} 
+      />
+      <CustomButton
+        title={"Voltar"}
+        onPress={() => { navigation.goBack(); }}
+      />
+
     </SafeAreaView>
   );
 }
